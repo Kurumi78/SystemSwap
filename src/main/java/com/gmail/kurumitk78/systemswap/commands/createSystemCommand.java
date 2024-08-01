7811785ecc19c27e27e85f14b0b57b988e4de1e6
Kurumi78 <kurumitk78@gmail.com>
2024-08-01 14:38:43 -0500

    disallow system creation if player already has system registered

--- src/main/java/com/gmail/kurumitk78/systemswap/commands/createSystemCommand.java
+++ src/main/java/com/gmail/kurumitk78/systemswap/commands/createSystemCommand.java
@@ -14,12 +14,21 @@ public class createSystemCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
         if(commandSender instanceof Player) {
-             UUID returnedUUID = SystemSwap.createSystem(((Player) commandSender).getUniqueId());
-            commandSender.sendMessage("System created using unique ID" + returnedUUID.toString());
+            if(SystemSwap.getSystemFromPlayerUUID(((Player) commandSender).getUniqueId()) == null){
+                UUID returnedUUID = SystemSwap.createSystem(((Player) commandSender).getUniqueId());
+                commandSender.sendMessage("System created using unique ID" + returnedUUID.toString());
+                return false;
+            }
+            else{
+                commandSender.sendMessage("You already have a system created.");
+                return false;
+            }
+
         }
-        else{
+        else{ //Else for chekcing if player
             Bukkit.getLogger().log(Level.WARNING, "Only a player may use this command");
+            return false;
         }
-        return false;
+
     }
 }
