e0da3ecc3569d30e9948b7ae1f5886dc32af38d7
Kurumi78 <kurumitk78@gmail.com>
2024-08-01 13:15:00 -0500

    Add basic System object

--- src/main/java/com/gmail/kurumitk78/systemswap/System.java
+++ src/main/java/com/gmail/kurumitk78/systemswap/System.java
@@ -1,4 +1,25 @@
 package com.gmail.kurumitk78.systemswap;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class System {
+
+    private HashMap<String, Alter> Alters = new HashMap<String, Alter>();
+    private String systemUUID;
+    private Alter fronter;
+
+
+
+    public Alter getAlter(String uuid){
+        return Alters.getOrDefault(uuid, null);
+    }
+    public HashMap getAlterList(){
+        return Alters;
+    }
+    public String getSystemUUID(){
+        return systemUUID;
+    }
+
+
 }
