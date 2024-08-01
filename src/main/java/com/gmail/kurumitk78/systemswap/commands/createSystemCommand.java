e1c24a3150bd0f9f4b570e47b139950829f3e7cc
Kurumi78 <kurumitk78@gmail.com>
2024-08-01 14:33:27 -0500

    add implemenmtation to command to create system

--- src/main/java/com/gmail/kurumitk78/systemswap/commands/createSystemCommand.java
+++ src/main/java/com/gmail/kurumitk78/systemswap/commands/createSystemCommand.java
@@ -1,4 +1,25 @@
 package com.gmail.kurumitk78.systemswap.commands;
 
-public class createSystemCommand {
+import com.gmail.kurumitk78.systemswap.SystemSwap;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
+import java.util.logging.Level;
+
+public class createSystemCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
+        if(commandSender instanceof Player) {
+             UUID returnedUUID = SystemSwap.createSystem(((Player) commandSender).getUniqueId());
+            commandSender.sendMessage("System created using unique ID" + returnedUUID.toString());
+        }
+        else{
+            Bukkit.getLogger().log(Level.WARNING, "Only a player may use this command");
+        }
+        return false;
+    }
 }
